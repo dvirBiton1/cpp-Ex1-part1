8530e1fd134bed7d02f9e6b9782dc974135a3fce
Dvir <dvir199710@gmail.com>
2022-03-12 20:22:04 +0200

    finnal with mat

--- mat.cpp
+++ mat.cpp
@@ -1,39 +1,56 @@
 #include "mat.hpp"
 #include <string>
 #include <iostream>
-#include <algorithm>
+// #include <algorithm>
 #include <stdexcept>
+#include <vector>
+// #include <typeinfo>
+// #include <ctype.h>
 
 using namespace std;
-namespace ariel
-{
+namespace ariel{
+
     /*
     that func get 2 num and 2 symbol שnd if indeed meets the conditions 
     the func return string(mat with the two symbol).
     */
     string mat(int col, int row, char s1, char s2)
     {
+        vector<vector<int>> matrix(row, vector<int>(col));
+        int const minChar = 33;
+        int const maxChar = 126;
+        int number = 1;
         if (row <= 0 || col <= 0)
         {
             throw runtime_error("Mat size is unegative or zero");
         }
-        else if (row % 2 == 0 || col % 2 == 0)
+        if (row % 2 == 0 || col % 2 == 0)
         {
             throw runtime_error("Mat size is always odd");
         }
-        else if (s1 < 33 || s2 < 33 || s1 > 126 || s2 >126)
+        // if (typeid(col).name() != typeid(number).name() || typeid(row).name() != typeid(number).name())
+        // {
+        //     throw runtime_error("problame with the type of int");
+        // }
+        if (!(s1 >= minChar && s1 <= maxChar))
         {
             throw runtime_error("unvalid symbol");
         }
-        string outMat = "";
-        int **myMat1 = new int *[row];
-        int **myMat2 = new int *[row];
-        for (int i = 0; i < row; ++i)
+        if (!(s2 >= minChar && s2 <= maxChar))
         {
-            myMat1[i] = new int[col];
-            myMat2[i] = new int[col];
-
+            throw runtime_error("unvalid symbol");
         }
+        string outMat;
+        // int **myMat1 = new int *[row];
+        // int **myMat2 = new int *[row];
+        // for (int i = 0; i < row; ++i)
+        // {
+        //     myMat1[i] = new int[col];
+        //     myMat2[i] = new int[col];
+
+        // }
+        vector<vector<int>> myMat1(row, vector<int>(col));
+        vector<vector<int>> myMat2(row, vector<int>(col));
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
@@ -68,7 +85,7 @@ namespace ariel
         {
             for (int j = 0; j < col; j++)
             {
-                if (!(myMat1[i][j] % 2))
+                if ((myMat1[i][j] % 2) == 0)
                 {
                     outMat += s1;
                 }
@@ -82,13 +99,13 @@ namespace ariel
                 outMat += '\n';
             }
         }
-        for (int i = 0; i < row; ++i)
-        {
-            delete[] myMat1[i];
-            delete[] myMat2[i];
-        }
-        delete[] myMat1;
-        delete[] myMat2;
+        // for (int i = 0; i < row; ++i)
+        // {
+        //     delete[] myMat1[i];
+        //     delete[] myMat2[i];
+        // }
+        // delete[] myMat1;
+        // delete[] myMat2;
         // cout << "out mat:\n"
         //      << outMat;
         return outMat;
--- mat.hpp
+++ mat.hpp
@@ -1,9 +1,39 @@
-#include<string>
-#include<iostream>
+#include <string>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 namespace ariel
 {
     string mat(int col, int row, char s1, char s2);
 };
+class Matrix {
+ public:
+  Matrix(): data({}) {}
 
+  Matrix(const int &rows, const int &cols) {
+    Reset(rows, cols);
+  }
+
+  void Reset(const int &rows, const int &cols) {
+    data.resize(rows);
+    for (int i = 0; i < rows; ++i) {
+      data.at(i).resize(cols);
+    }
+  }
+
+  int GetNumRows() const {
+    return data.size();
+  }
+
+  int GetNumColumns() const {
+    if (GetNumRows() > 0) {
+      return data[0].size();
+    }
+
+    return 0;
+  }
+
+ private:
+  std::vector<std::vector<int>> data;
+};
